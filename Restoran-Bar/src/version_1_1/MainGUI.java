4b916aab24d2a588db32affde53b5799338cb954
Boyar Roman <br777roman@gmail.com>
2016-04-03 19:08:28 +0300

    commit 5  - переделанное правильно е UTF -8 и INI файлы

--- Restoran-Bar/src/version_1_1/MainGUI.java
+++ Restoran-Bar/src/version_1_1/MainGUI.java
@@ -5,8 +5,6 @@ import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -29,50 +27,41 @@ import javax.swing.border.BevelBorder;
  
 public class MainGUI extends JFrame {
 
-	// Для Парсинга------------------------------------------------------------
+ 
+ 	final public Vector<String> namesOfButtonsGorizontMenus = new Vector<String>();
 
-	// Вектор имен горизонтального меню
-	final public Vector<String> namesOfButtonsGorizontMenus = new Vector<String>();
+ 	final public TreeSet<String> mainMenuBluda = new TreeSet();	 
 
-	//основные блюда Меню Ресторана, TreeSet для автоматической сортировки по алфавиту
-	final public TreeSet<String> mainMenuBluda = new TreeSet();	 
+ 	final public Map<String,Vector<Menu>> mapGruppMenuBluda = new TreeMap<>();
 
-	// Меп блюд принадлежащих к одноименным группам (ключ = название подменю, value = список элентов данного подменю)
-	final public Map<String,Vector<Menu>> mapGruppMenuBluda = new TreeMap<>();
-
-	//основные пункты меню СТОЛЫ,TreeSet для автоматической сортировки по алфавиту
-	final public TreeSet<String> mainMenuStoli = new TreeSet();	 
+ 	final public TreeSet<String> mainMenuStoli = new TreeSet();	 
 	
-	// Меп (ключ = название подменю, value = список элентов данного подменю)
-	//Vector<String> -чтобы порядок добавления сохранялся и у пользователя была возможность 
-	//самому влиять на порядок доавления
-	final public Map<String,Vector<String>> mapGruppMenuStoli = new TreeMap<>();
+ 	final public Map<String,Vector<String>> mapGruppMenuStoli = new TreeMap<>();
 	
 	
 	
-	// Для отрисовки ГУИ--------------------------------------------------
-	// Запомнить размер экрана 
+	//  --------------------------------------------------
+	 
 	final int x_screenSize = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 	final int y_screenSize = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
-	// размер окна
+	 
 	int x_size = 800; int y_size = 400;
 
-	//конструктор JFrame
+	// JFrame
 	public MainGUI(String pathToFileGorizontMenuNames, String pathTOspisok_menu,String pathTOstoli_menu)
 			throws ClassNotFoundException, InstantiationException,
 			IllegalAccessException, UnsupportedLookAndFeelException {
  		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-		setTitle("Меню системы РЕСТОРАН-БАР");
-		setDefaultCloseOperation(EXIT_ON_CLOSE); // Диспоуз - закрыть прогу после закрытия ПОСЛЕДНЕГО ОКНА
-
-		setLayout(new FlowLayout()); // включить ФлоуЛейаут() - простейший, размещает компоненты слева направо
+		setTitle("Р РµСЃС‚РѕСЂР°РЅ-Р‘Р°СЂ СЃРёСЃС‚РµРјР°");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);  
+		setLayout(new FlowLayout());  
 		setSize(x_size, y_size);
-		//размещение окна ГУИ по центру экрана
+ 
 		setLocation((x_screenSize - x_size) / 2, (y_screenSize - y_size) / 2);
 		drawJMenuButtonsInGUI(pathToFileGorizontMenuNames,
-				        pathTOspisok_menu,pathTOstoli_menu);// нарисовать все компоненты из файла "filePath"
+				        pathTOspisok_menu,pathTOstoli_menu); 
 
 		setVisible(true);
  		  
@@ -88,84 +77,73 @@ public class MainGUI extends JFrame {
 					"d:\\MyTeamProject\\IO\\stoli_menu.ini");
 	}
 	
-	//парсер горизонтального меню в namesOfButtonsGorizontMenus
+	//  namesOfButtonsGorizontMenus
 	public Vector<String> parserReadMainButtonsMenu(String pathToFileGorizontMenuNames) throws IOException {
-		// читаем БуфРидером и накапливаем необходимую инфу
+		
+		
 		BufferedReader buffReader = new BufferedReader(new FileReader(pathToFileGorizontMenuNames));
 		String str = "";
 
-		// чтение до тех пор пока строка не нулевая
-		for (; (str = buffReader.readLine()) != null;) {
-			// пустую строку не обраб
-			if (str.trim().equals(""))
-				continue;
-			// если сстрока содержит КОНЕЦ_ФАЙЛА - прекратить чтение
-			if (str.contains("#КОНЕЦ_ФАЙЛА!")) {
+ 		for (; (str = buffReader.readLine()) != null;) {
+ 			if (str.trim().equals(""))	continue;
+ 			if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) {
 				break;
 			}// if
 
-			// МЕНЮ
-			// ГОРИЗОНТАЛЬНОЕ===============================================
-			else if (str.contains("ГоризонтМеню:")) {
-				str = str.substring("ГоризонтМеню:".length()).trim();
+		 
+ 			else if (str.contains("Р“РѕСЂРёР·РѕРЅС‚РњРµРЅСЋ:")) {
+				str = str.substring("Р“РѕСЂРёР·РѕРЅС‚РњРµРЅСЋ:".length()).trim();
 				StringTokenizer tokzer = new StringTokenizer(str, ",");
 
-				// заносим в коллекцию namesGorizontMenus пункты главного меню
+				// namesGorizontMenus 
 				while (tokzer.countTokens() > 0) {
-					String str_temp = (String) tokzer.nextElement().toString()
-							.trim();
+					String str_temp = (String) tokzer.nextElement().toString().trim();
 					// System.out.print ("|" + str_temp );
 					namesOfButtonsGorizontMenus.add(str_temp);
 				}
 				// System.out.println( );
 
-			}// else горизонт меню
+			}// else  
  
-		}// for цикд чтения всего файла
+		}// for 
 		buffReader.close();
 		return namesOfButtonsGorizontMenus;
 	}
 	
-	//чтение меню ресторана в Меп mapGruppMenu 
+	// mapGruppMenu 
 	public  Map<String,Vector<Menu>> parserReadPointsOfMenuBluds(String pathTOspisok_menu) throws IOException{
-/*
-		МИНИ ИНСТРУКЦИЯ ПО ОФОРМЛЕНИЮ ДОКУМЕНТА
-		 1. ### - указатель завершения блока
-		 2. #Основные позиции меню: неизменяемое название
-		 3. #Группа_"НАЗВАНИЕ ГРУППЫ МЕНЮ" (тут после #Группа_ -следует писать точное название группы меню, 
-		    которое должно совпадать с одним из пунктов основого меню!
-		 4. #КОНЕЦ_ФАЙЛА! - неизменяемая запись, которая указывает на окончание файла
-		 5. Пример "АНТЛАНТИЧЕСКАЯ СЕЛЬДЬ = 200(гривен)/100(грамм)"  указывает, что:
-		     1. после названия блюда должно всегда стоять "="
-		     2. первым указвается число стоимости блюда в гривнах
-		     3. вторым вес блюда в граммах
-		     4. все числа только целые
-		     5. формат записи через "/" неизменный,
-		     6. можно менять только числа. Например: "280(гривен)/420(грамм)"
-		 6. символ "//" - означает игнорировать данную запись в машинном коде 
-*/
+		//РјРёРЅРё РёРЅСЃС‚СЂСѓРєС†РёСЏ РїРѕ РѕС„РѕСЂРјР»РµРЅРёСЋ РґРѕРєСѓРјРµРЅС‚Р°
+		// 1. ### - СѓРєР°Р·Р°С‚РµР»СЊ Р·Р°РІРµСЂС€РµРЅРёСЏ Р±Р»РѕРєР°
+		// 2. #РћСЃРЅРѕРІРЅС‹Рµ РїРѕР·РёС†РёРё РјРµРЅСЋ: РЅРµРёР·РјРµРЅСЏРµРјРѕРµ РЅР°Р·РІР°РЅРёРµ
+		// 3. #Р“СЂСѓРїРїР°_"РќРђР—Р’РђРќРР• Р“Р РЈРџРџР« РњР•РќР®" (С‚СѓС‚ РїРѕСЃР»Рµ #Р“СЂСѓРїРїР°_ -СЃР»РµРґСѓРµС‚ РїРёСЃР°С‚СЊ С‚РѕС‡РЅРѕРµ РЅР°Р·РІР°РЅРёРµ РіСЂСѓРїРїС‹ РјРµРЅСЋ, 
+//		    РєРѕС‚РѕСЂРѕРµ РґРѕР»Р¶РЅРѕ СЃРѕРІРїР°РґР°С‚СЊ СЃ РѕРґРЅРёРј РёР· РїСѓРЅРєС‚РѕРІ РѕСЃРЅРѕРІРѕРіРѕ РјРµРЅСЋ!
+		// 4. #РљРћРќР•Р¦_Р¤РђР™Р›Рђ! - РЅРµРёР·РјРµРЅСЏРµРјР°СЏ Р·Р°РїРёСЃСЊ, РєРѕС‚РѕСЂР°СЏ СѓРєР°Р·С‹РІР°РµС‚ РЅР° РѕРєРѕРЅС‡Р°РЅРёРµ С„Р°Р№Р»Р°
+		// 5. РџСЂРёРјРµСЂ "РђРќРўР›РђРќРўРР§Р•РЎРљРђРЇ РЎР•Р›Р¬Р”Р¬ = 200(РіСЂРёРІРµРЅ)/100(РіСЂР°РјРј)"  СѓРєР°Р·С‹РІР°РµС‚, С‡С‚Рѕ:
+//		     1. РїРѕСЃР»Рµ РЅР°Р·РІР°РЅРёСЏ Р±Р»СЋРґР° РґРѕР»Р¶РЅРѕ РІСЃРµРіРґР° СЃС‚РѕСЏС‚СЊ "="
+//		     2. РїРµСЂРІС‹Рј СѓРєР°Р·РІР°РµС‚СЃСЏ С‡РёСЃР»Рѕ СЃС‚РѕРёРјРѕСЃС‚Рё Р±Р»СЋРґР° РІ РіСЂРёРІРЅР°С…
+//		     3. РІС‚РѕСЂС‹Рј РІРµСЃ Р±Р»СЋРґР° РІ РіСЂР°РјРјР°С…
+//		     4. РІСЃРµ С‡РёСЃР»Р° С‚РѕР»СЊРєРѕ С†РµР»С‹Рµ
+//		     5. С„РѕСЂРјР°С‚ Р·Р°РїРёСЃРё С‡РµСЂРµР· "/" РЅРµРёР·РјРµРЅРЅС‹Р№,
+//		     6. РјРѕР¶РЅРѕ РјРµРЅСЏС‚СЊ С‚РѕР»СЊРєРѕ С‡РёСЃР»Р°. РќР°РїСЂРёРјРµСЂ: "280(РіСЂРёРІРµРЅ)/420(РіСЂР°РјРј)"
+		// 6. СЃРёРјРІРѕР» "//" - РѕР·РЅР°С‡Р°РµС‚ РёРіРЅРѕСЂРёСЂРѕРІР°С‚СЊ РґР°РЅРЅСѓСЋ Р·Р°РїРёСЃСЊ РІ РјР°С€РёРЅРЅРѕРј РєРѕРґРµ 
 	
 	
-		//Парсер INI файла для порлучений Инфы о меню
-			// читаем БуфРидером и накапливаем необходимую инфу
+		 
 		BufferedReader buffReader = new BufferedReader(new FileReader(pathTOspisok_menu));
 		String str = "";
 
-		// чтение до тех пор пока строка не нулевая
-		for (; (str = buffReader.readLine()) != null;) {
+ 		for (; (str = buffReader.readLine()) != null;) {
 			
-			// пустую строку не обраб
-			if (str.trim().equals(""))	continue;			
+ 			if (str.trim().equals(""))	continue;			
 			if (str.startsWith("//"))	continue;
 			
-			// если сстрока содержит КОНЕЦ_ФАЙЛА - прекратить чтение
-			if (str.contains("#КОНЕЦ_ФАЙЛА!")) 	break;
+ 			if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) 	break;
 	 			
-			if (str.contains("#Основные позиции меню:")) {
+			if (str.contains("#РћСЃРЅРѕРІРЅС‹Рµ РїРѕР·РёС†РёРё РјРµРЅСЋ:")) {
  				
 				while(!(str= buffReader.readLine()).contains("###") ){
 					System.out.println(str);
-					if (str.contains("#КОНЕЦ_ФАЙЛА!")) 	break;
+					if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) 	break;
 					if (str.trim().equals(""))	continue;
 					if (str.startsWith("//"))	continue;
 
@@ -174,13 +152,13 @@ public class MainGUI extends JFrame {
 			}//if
 //			System.out.println("str  ="+str);
 	
-			if (str.contains("#Группа_")) {
- 				String key = str.substring("#Группа_".length(), str.indexOf(":"));
+			if (str.contains("#Р“СЂСѓРїРїР°_")) {
+ 				String key = str.substring("#Р“СЂСѓРїРїР°_".length(), str.indexOf(":"));
  				Vector<String> vectorTemp = new Vector<String>();
  				Vector<Menu> vectorMenu = new Vector< >();
 				while(!(str= buffReader.readLine()).contains("###") ){
 //					System.out.println(str);
-					if (str.contains("#КОНЕЦ_ФАЙЛА!")) 	break;
+					if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) 	break;
 					if (str.trim().equals(""))	continue;
 					if (str.startsWith("//"))	continue;
 					 
@@ -193,45 +171,41 @@ public class MainGUI extends JFrame {
 						weightOfElementOfMenu = Integer.valueOf((str.substring(str.indexOf("/")+1,str.lastIndexOf("(")).trim()));
 					} catch (java.lang.StringIndexOutOfBoundsException e) {
 
-						System.out.println("Не все поля заполнены");
+						System.out.println("РќРµ РІРµСЂРЅС‹Р№ С„РѕСЂРјР°С‚ Р·Р°РїРёСЃРё С†РµРЅС‹");
 					}
 					
-					Menu.count_uniq++;//нарасчиваем уникальный код блюда
+					Menu.count_uniq++; 
 					vectorMenu.add(new Menu(nameOfElementOfMenu, costOfElementOfMenu, weightOfElementOfMenu));
 					vectorTemp.add(str.trim());
 				}//while
 				mapGruppMenuBluda.put(key, vectorMenu);
 			}//if
 						
-		}// for цикд чтения всего файла
+		}// for
 	
  		buffReader.close();
 		return mapGruppMenuBluda;
 	}//parserReadPointsOfMenu
 			
- 	//чтение меню Столы в Меп mapGruppMenuStoli 
+ 	//mapGruppMenuStoli 
 	public  Map<String,Vector<Menu>> parserReadStolPoints(String pathTOstoli_menu) throws IOException{
  	
-		//Парсер INI файла для порлучений Инфы о меню
-			// читаем БуфРидером и накапливаем необходимую инфу
-		BufferedReader buffReader = new BufferedReader(new FileReader(pathTOstoli_menu));
+		 
+ 		BufferedReader buffReader = new BufferedReader(new FileReader(pathTOstoli_menu));
 		String str = "";
 
-		// чтение до тех пор пока строка не нулевая
-		for (; (str = buffReader.readLine()) != null;) {
+ 		for (; (str = buffReader.readLine()) != null;) {
 			
-			// пустую строку не обраб
-			if (str.trim().equals(""))	continue;			
+ 			if (str.trim().equals(""))	continue;			
 			if (str.startsWith("//"))	continue;
 			
-			// если сстрока содержит КОНЕЦ_ФАЙЛА - прекратить чтение
-			if (str.contains("#КОНЕЦ_ФАЙЛА!")) 	break;
+ 			if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) 	break;
 	 			
-			if (str.contains("#Столы:")) {
+			if (str.contains("#РЎС‚РѕР»С‹:")) {
  				
 				while(!(str= buffReader.readLine()).contains("###") ){
 					System.out.println(str);
-					if (str.contains("#КОНЕЦ_ФАЙЛА!")) 	break;
+					if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) 	break;
 					if (str.trim().equals(""))	continue;
 					if (str.startsWith("//"))	continue;
 
@@ -240,12 +214,12 @@ public class MainGUI extends JFrame {
 			}//if
 //			System.out.println("str  ="+str);
 	
-			if (str.contains("#Группа_")) {
- 				String key = str.substring("#Группа_".length(), str.indexOf(":"));
+			if (str.contains("#Р“СЂСѓРїРїР°_")) {
+ 				String key = str.substring("#Р“СЂСѓРїРїР°_".length(), str.indexOf(":"));
  				Vector<String> vectorTemp = new Vector();
  				while(!(str= buffReader.readLine()).contains("###") ){
 //					System.out.println(str);
-					if (str.contains("#КОНЕЦ_ФАЙЛА!")) 	break;
+					if (str.contains("#РљРћРќР•Р¦_Р¤РђР™Р›Рђ!")) 	break;
 					if (str.trim().equals(""))	continue;
 					if (str.startsWith("//"))	continue;
 					 
@@ -258,66 +232,60 @@ public class MainGUI extends JFrame {
 //						weightOfElementOfMenu = Integer.valueOf((str.substring(str.indexOf("/")+1,str.lastIndexOf("(")).trim()));
 //					} catch (java.lang.StringIndexOutOfBoundsException e) {
 //
-//						System.out.println("Не все поля заполнены");
+//						System.out.println("пїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
 //					}
 					
-//					Menu.count_uniq++;//нарасчиваем уникальный код блюда
+//					Menu.count_uniq++; 
  					vectorTemp.add(str.trim());
 				}//while
 				mapGruppMenuStoli.put(key, vectorTemp);
 			}//if
 						
-		}// for цикд чтения всего файла
+		}// for  
 	
  		buffReader.close();
 		return mapGruppMenuBluda;
 	}//parserReadPointsOfMenu	//----------------------------
 		 
-	// метод который рисует необходимые кнопки из инфы полученной после парсинга
+	 
 	public void drawJMenuButtonsInGUI(	String pathToFileGorizontMenuNames,
 										String pathTOspisok_menu,
 										String pathTOstoli_menu) {
 		 
-			// прочитать файл
+			// 
 			try { parserReadMainButtonsMenu(pathToFileGorizontMenuNames);} 
 			catch (IOException e1) {JOptionPane.showMessageDialog(null,
-						"К сожалению указанного INI файла для формирования основной строки меню нет!");}
+						"РЅРµС‚ С‚Р°РєРѕРіРѕ РРќР С„Р°Р№Р»Р° РѕСЃРЅРѕРІРЅРѕРіРѕ РјРµРЅСЋ!");}
 			try { 	parserReadPointsOfMenuBluds(pathTOspisok_menu);	} 
 			catch (IOException e) {JOptionPane.showMessageDialog(null,
-					"К сожалению такого INI файла Меню Ресторана Нет!");} 
+					"РЅРµС‚ С‚Р°РєРѕРіРѕ РРќР С„Р°Р№Р»Р° СЃРїРёСЃРєР° РїРѕРґРјРµРЅСЋ!");} 
 			
 			try { 	parserReadStolPoints( pathTOstoli_menu);}
 			catch (IOException e) {JOptionPane.showMessageDialog(null, 
-					"К сожалению таокго INI файла пункта Столы Нет!");} 
+					"РЅРµС‚ С‚Р°РєРѕРіРѕ РРќР С„Р°Р№Р»Р° СЃС‚РѕР»РѕРІ РјРµРЅСЋ!");} 
 			
 
-			// ВЕРХНЕЕ ГОРИЗОНТАЛЬНОЕ МЕНЮ - полоска меню - менюБар
-			JMenuBar menuBar = new JMenuBar();
+ 			JMenuBar menuBar = new JMenuBar();
 
-			// если есть хоть один пункт гориз меню
-			if (! namesOfButtonsGorizontMenus.isEmpty()) {
+ 			if (! namesOfButtonsGorizontMenus.isEmpty()) {
 
-				// присоединить объект МенюБара к окну ДжФреймовские
-				setJMenuBar(menuBar);
+ 				setJMenuBar(menuBar);
 
-				// цикл создания Горизонтальных Менюшек
-				for (String elem_goriz_menu :  namesOfButtonsGorizontMenus) {
+ 				for (String elem_goriz_menu :  namesOfButtonsGorizontMenus) {
 
 					//
 					JMenu menuGorizontalnoe = new JMenu(elem_goriz_menu);
 					menuGorizontalnoe.setFocusPainted(false);
 					menuGorizontalnoe.setPreferredSize(new Dimension(x_size/namesOfButtonsGorizontMenus.size(), 1));
 					menuGorizontalnoe.setBorder(new BevelBorder(BevelBorder.RAISED));
-					// установить разделитель
-					menuBar.add(new JSeparator(SwingConstants.VERTICAL));
+ 					menuBar.add(new JSeparator(SwingConstants.VERTICAL));
 					menuBar.add(menuGorizontalnoe);
 
 					//===============================================================
 				
- 					// для меню Ресторана
-					if (mainMenuBluda.size()>0 & elem_goriz_menu.equals("Меню Ресторана") )  {
-						// Цикл создания Вертикальных менюшек
-						for (String elem_vertik_menu : mainMenuBluda) {
+ 					if (mainMenuBluda.size()>0 & elem_goriz_menu.equals("РњРµРЅСЋ Р РµСЃС‚РѕСЂР°РЅР°") )  {
+ 
+ 						for (String elem_vertik_menu : mainMenuBluda) {
 							// System.out.println("elem_namesVertik_menu = "
 							// +elem_vertik_menu);
 
@@ -327,15 +295,14 @@ public class MainGUI extends JFrame {
 
 								JMenu subMenu = new JMenu(elem_vertik_menu);
 
-								//вот тут добавляем пункты каждой группы 
+							 
 								for (Menu elem_podmenu : mapGruppMenuBluda.get(elem_vertik_menu)) {
  									
 									subMenu.add(elem_podmenu.nameOfElementOfMenu+ " - ("+
-											elem_podmenu.costOfElementOfMenu+" гривен " +"/"+
-											elem_podmenu.weightOfElementOfMenu+ " грамм)");
+											elem_podmenu.costOfElementOfMenu+" РіСЂРёРІРµРЅ " +"/"+
+											elem_podmenu.weightOfElementOfMenu+ " РіСЂР°РјРј)");
 
-								}// foreach по подменюшкам
-
+								}// foreach  
 								menuGorizontalnoe.add(subMenu);
 
 							} else {
@@ -346,32 +313,32 @@ public class MainGUI extends JFrame {
 							}
 
 							
-						}// foreach создания Вертикальных менюшек
+						}// foreach  
 
 					}// if (ParserReadMenuFromFile.namesVertikMenus.get(elem)!=null)
 					//===============================================================
 				
 					
-					// для меню Столы
-					else if (mainMenuStoli.size()>0 & elem_goriz_menu.equals("Столы") )  {
-						// Цикл создания Вертикальных менюшек
+					 
+					else if (mainMenuStoli.size()>0 & elem_goriz_menu.equals("РЎС‚РѕР»С‹") )  {
+
 						for (String elem_vertik_stoli_menu : mainMenuStoli) {
 							  System.out.println("elem_vertik_stoli_menu = "
 							  +elem_vertik_stoli_menu);
 
-							  //Если уданной группы меню есть подпунткы
+
 							if (mapGruppMenuStoli.get(elem_vertik_stoli_menu).size() != 0) {
 
 								menuGorizontalnoe.add(new JSeparator());
 
 								JMenu subMenu = new JMenu(elem_vertik_stoli_menu);
 
-								//вот тут добавляем пункты каждой группы 
+
 								for (String elem_podmenu : mapGruppMenuStoli.get(elem_vertik_stoli_menu)) {
 									
 									subMenu.add(elem_podmenu).addActionListener(new MyActionListener()); ;
 
-								}// foreach по подменюшкам
+								}// foreach
 
 								
 								if (subMenu.getSize()!=null) {
@@ -382,7 +349,7 @@ public class MainGUI extends JFrame {
 
 							} 
 							
-							//есди у группы подпунктов нет
+
 							else {
 								JMenuItem menuItem = new JMenuItem(elem_vertik_stoli_menu);
 								menuGorizontalnoe.add(new JSeparator());
@@ -391,30 +358,29 @@ public class MainGUI extends JFrame {
 							}
 
 							
-						}// foreach создания Вертикальных менюшек
+						}// foreach 
+
 
 					}// if (ParserReadMenuFromFile.namesVertikMenus.get(elem)!=null)
 					//===============================================================
-					// когда у пункта гориз меню нет подпукта
+
 					else {
-						;
-						 System.out.println("пустой");
+						
+						 System.out.println("РЎС‡РёС‚С‹РІР°РЅРёРµ РѕСЃРЅРѕРІРЅРѕРіРѕ РјРµРЅСЋ Р·Р°РєРѕРЅС‡РµРЅРѕ");
 					}// else
 
-				}// foreach создания Горизонтальных Менюшек
+				}// foreach
+
 
 			}// if
 			else {
 				JOptionPane.showMessageDialog(null,
-								"К сожалению Вы не создали ни одного пункта главного меню!");
+								"Рљ СЃРѕР¶Р°Р»РµРЅРёСЋ, РЅРµС‚ РЅРё РѕРґРЅРѕРіРѕ РїСѓРЅРєС‚Р° РѕСЃРЅРѕРІРЅРѕРіРѕ РјРµРЅСЋ Р“РЈР РѕРєРЅР°!");
 				System.exit(1);
 			}
 	 
 	 
-			// установки размера - setPreferredSize
-			menuBar.setPreferredSize(new Dimension(1, 50)); // высота работает, а длина нет - длина
-																// МепнюБара считается ВСЕ окно
-	 				 
+ 			menuBar.setPreferredSize(new Dimension(1, 50));  
 		 
 		}// drawJMenuButtonsInGUI
 		
@@ -428,8 +394,8 @@ public class MainGUI extends JFrame {
 					
 			System.out.println(e.getActionCommand());
 			
-			if(e.getActionCommand().equals("создать новый заказ")){
-							new GUI_zakaz("создание нового заказа");
+			if(e.getActionCommand().equals("СЃРѕР·РґР°С‚СЊ РЅРѕРІС‹Р№ Р·Р°РєР°Р·")){
+							new GUI_zakaz("СЃРѕР·РґР°С‚СЊ РЅРѕРІС‹Р№ Р·Р°РєР°Р·");
 			}
 				
 			 
